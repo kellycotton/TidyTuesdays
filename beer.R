8a8d6d882a0e718c8ea1f46d4fd7e1dc8e655e3f
kellycotton <42971192+kellycotton@users.noreply.github.com>
2020-04-01 14:15:55 -0400

    update beer.r

--- beer.R
+++ beer.R
@@ -36,27 +36,34 @@ beer_states$region <- factor(beer_states$region,
                                          "Delta States","Lake States","Northern Plains",
                                          "Southern Plains","Mountain","Pacific"))
 
+#Beer Production by State----
+#Find mean beer production by state
+beer <- beer_states %>%
+  group_by(state,region) %>%
+  summarize(mean_barrels = mean(barrels,na.rm = TRUE))
+
+#Plot average annual beer production for each state
+png("beermap.png",units="in",width=9,height=4,res=300)
+print (
+  plot_usmap(data = beer, values = "mean_barrels") + 
+    scale_fill_viridis(name="Barrels Produced",label = scales::comma) + 
+    theme(legend.position = "right") + ggtitle("Annual Barrels of Beer Produced")
+)
+dev.off()
+
+#Beer and Tuition----
 #Find the mean tuition by state
 tuition <- tuition_cost %>%
   group_by(state_code) %>%
   summarize(mean_tuition = mean(in_state_tuition)) %>%
   rename(state=state_code)
-
-#Find mean beer production by state
-beer <- beer_states %>%
-  group_by(state,region) %>%
-  summarize(mean_barrels = mean(barrels,na.rm = TRUE))
   
 #Combine beer and tuition dataframes
 beer_tuition <- merge(beer,tuition,by="state")
 
-#Beer Production by State----
-#Plot average annual beer production for each state
-plot_usmap(data = beer_tuition, values = "mean_barrels") + 
-  scale_fill_viridis(name="Mean Barrels Produced",label = scales::comma) + 
-  theme(legend.position = "right") + ggtitle("Average Annual Barrels Produced")
-
 #Plot average tuition vs. average amount of beer produced by state and region
+png("beertution1.png",units="in",width=9,height=5,res=300)
+print (
 ggplot(beer_tuition) + aes(x=mean_barrels,y=mean_tuition,color=region) +
   geom_jitter(size=4) + 
   scale_x_log10(label = scales::comma) +
@@ -72,14 +79,19 @@ ggplot(beer_tuition) + aes(x=mean_barrels,y=mean_tuition,color=region) +
                   point.padding = 0.7,
                   segment.color = 'grey50',
                   color="black") 
-
+)
+dev.off()
 #Plot average tuition for each state, average amount of beer produced is the size of the dot
+png("beertuition2.png",units="in",width=9,height=7,res=300)
+print (
 ggplot(beer_tuition) + aes(x=reorder(state,mean_tuition),y=mean_tuition,size=mean_barrels,color=region) +
   geom_point()  + coord_flip() + scale_color_viridis(discrete=TRUE) +
   scale_size_continuous(name="Annual Barrels Produced",label = scales::comma) +
   labs(y="Annual In-State Tuition ($)",x="State", color="Region",
        title="Average College Tuition vs. How Much Beer is Being Produced") +
   theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust=0.5)) 
+)
+dev.off()
 
 #Plot this data for kegs only 
 
@@ -93,11 +105,13 @@ kegs <- beer_states %>%
 kegs_tuition <- merge(kegs,tuition,by="state")
 
 #Plot average tuition vs. average amount of beer kegs produced by state and region
+png("kegtuition.png",units="in",width=9,height=4,res=300)
+print(
 ggplot(kegs_tuition) + aes(x=mean_barrels,y=mean_tuition,color=region) +
   geom_point(size=4)  + 
   scale_x_log10(label = scales::comma) +
   scale_color_viridis(discrete = TRUE) +
-  labs(y="Mean Annual In-State Tuition ($)",x="Annual Barrels Produced (Kegs)",
+  labs(y="Mean Annual In-State Tuition ($)",x="Annual Beer Produced (Barrels)",
        title="Average College Tuition vs. How Much Beer is Being Produced",color="Region")+
   theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust=0.5),
         plot.margin = margin(0.5,1,.5,.5,"cm")) +
@@ -107,7 +121,8 @@ ggplot(kegs_tuition) + aes(x=mean_barrels,y=mean_tuition,color=region) +
                   point.padding = 0.7,
                   segment.color = 'grey50',
                   color="black") 
-
+)
+dev.off()
 #Compare the beer data to dairy production
 
 #Find mean milk production
@@ -117,12 +132,14 @@ milk <- milk_states %>%
 
 milk_beer <- merge(milk,beer,by="state")
 
+png("milkbeer.png",units="in",width=9,height=4,res=300)
+print (
 ggplot(milk_beer) + aes(x=mean_barrels,y=mean_milk,color=region) +
   geom_point(size=4) +
   scale_x_log10(label = scales::comma) +
   scale_y_log10(label = scales::comma) +
   scale_color_viridis(discrete=TRUE) +
-  labs(y="Annual Milk Produced (Pounds)",x="Annual Barrels of Beer Produced",
+  labs(y="Annual Milk Produced (Pounds)",x="Annual Beer Produced (Barrels)",
        title="Milk and.Beer Production",color="Region") +
   theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust=0.5),
         plot.margin = margin(0.5,1,.5,.5,"cm")) +
@@ -132,7 +149,8 @@ ggplot(milk_beer) + aes(x=mean_barrels,y=mean_milk,color=region) +
                   point.padding = 0.7,
                   segment.color = 'grey50',
                   color="black") 
-
+)
+dev.off()
 
 # And UFOs...
 ufo_states <- ufo_sightings %>%
@@ -142,8 +160,10 @@ ufo_states <- ufo_sightings %>%
 
 ufo_beer <- merge(ufo_states,beer,by="state")
 
+png("ufobeer.png",units="in",width=9,height=4,res=300)
+print (
 ggplot(ufo_beer) + aes(x=mean_barrels,y=sightings,color=region) +
-  geom_point(size=4) + 
+  geom_point(size=3) + 
   scale_x_log10(label = scales::comma) +
   scale_color_viridis(discrete=TRUE) +
   labs(y="Number of UFO Sightings",x="Annual Barrels of Beer Produced",
@@ -152,8 +172,9 @@ ggplot(ufo_beer) + aes(x=mean_barrels,y=sightings,color=region) +
         plot.margin = margin(0.5,1,.5,.5,"cm")) +
   geom_text_repel(aes(label = state),
                   data = subset(ufo_beer, (sightings >=2500)),
-                  box.padding   = 0.4, 
-                  point.padding = 0.7,
+                  box.padding   = 0.5, 
+                  point.padding = 0.5,
                   segment.color = 'grey50',
                   color="black") 
-
+)
+dev.off()
