a19742850fcf1231765c993086e6afe45871ece8
Kelly Cotton <kcotton@gradcenter.cuny.edu>
2020-11-24 12:27:15 -0500

    add name generator

--- code/hiking.R
+++ code/hiking.R
@@ -15,11 +15,12 @@ names <- hike_data %>%
   anti_join(stop_words, by = c("name" = "word")) %>% #remove common words like and, the, etc.
   count(name)
 
+# Create plot
 names %>% 
-  slice_max(order_by = n, n = 25) %>% 
+  slice_max(order_by = n, n = 25) %>%  #only top 25 (actually 26) most common
   ggplot(aes(x = name, y = n, color = name, group = 1)) +
-  geom_line(size = 1) + geom_point(size = .3) +
-  geom_segment(aes(xend = name, y = 0, yend = n), linetype = "dotdash", alpha = .25) +
+  geom_line(size = 1) + geom_point(size = .3) + #add point to make it more rounded at the top
+  geom_segment(aes(xend = name, y = 0, yend = n), linetype = "dotdash", alpha = .25) + #geom_segment adds the dashed lines
   scale_color_manual(values = calecopal::cal_palette("arbutus", n = 26, type = "continuous")) +
   theme_ft_rc() +
   scale_y_continuous(breaks = c(50, 100, 150, 200, 250, 300, 350), name = "Number of Occurrences") +
@@ -34,3 +35,12 @@ names %>%
   
 #ggsave("Plots/trailnames.png", dpi = 300, width = 10)
   
+# Generate trail name
+name_generator <- function(n_words) {
+  words <- sample_n(names, n_words)[1]
+  new_name <- paste("Your personalized Washington State trail name is: ", sapply(words, paste, collapse = " "), "Trail.")
+  return (new_name)
+}
+
+# Enter your desired number of words 
+name_generator(4)
